0d9429b05cb07ee636e21efcc77958e7d807516b
otiai10 <otiai10@gmail.com>
2016-12-06 16:32:13 +0100

    Add minimum file and tests for it

---
+++ travis-ci-example/Foo.swift
@@ -0,0 +1,15 @@
+//
+//  File.swift
+//  travis-ci-example
+//
+//  Created by hiromu otiai on 12/6/16.
+//  Copyright © 2016 otiai10.com. All rights reserved.
+//
+
+import Foundation
+
+class Foo {
+    func sum(_ i: Int, _ j: Int) -> Int {
+        return i + j
+    }
+}
