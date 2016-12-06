1ef0129f10cb2ec9eb3b605f72b5a6b4a8c2e41c
otiai10 <otiai10@gmail.com>
2016-12-06 12:42:09 +0100

    Initial Commit

---
+++ travis-ci-example/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  travis-ci-example
+//
+//  Created by hiromu otiai on 12/6/16.
+//  Copyright © 2016 otiai10.com. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
